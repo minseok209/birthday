2cd10e34daf4b4bb3ae60ff121c05e354cf5190d
minseok209 <126706744+minseok209@users.noreply.github.com>
2024-02-20 20:32:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function updateTimer() {
 
 // 타이머를 업데이트하는 함수를 매초마다 호출
 setInterval(updateTimer, 1000);
+
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(function() {
+      var fadeOutText = document.querySelector('p.fade-out-text'); // 'fade-out-text' 클래스를 가진 <p> 요소 선택
+      fadeOutText.style.opacity = 0; // 5초 후 투명도를 0으로 변경
+  }, 5000); // 5000 밀리초(5초) 후 실행
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.caption').forEach(function(caption) {
+      caption.addEventListener('click', function() {
+          // data-target 속성에서 연결된 이미지의 ID를 가져옵니다.
+          var targetId = caption.getAttribute('data-target');
+          var targetImage = document.getElementById(targetId);
+          
+          // 이미지의 표시 상태를 토글합니다.
+          if (targetImage.classList.contains('hidden')) {
+              targetImage.classList.remove('hidden');
+              targetImage.classList.add('visible');
+          } else {
+              targetImage.classList.add('hidden');
+              targetImage.classList.remove('visible');
+          }
+      });
+  });
+});
+
+
